04371a067b03437d079d9a223a5c84b5e539ad63
Jirattikal42 <60840444+Jirattikal42@users.noreply.github.com>
2020-04-30 23:54:38 +0700

    Add files via upload

--- Pokemon.java
+++ Pokemon.java
@@ -1,51 +1,109 @@
-public class Pokemon{
-    
-    private String name;
-    private int lv;
-    /*private int HP,MP,atkDMG;
-    private int oldLV=1;*/
-    public Pokemon(String name){
-        this.name = name;
-        lv = 1;
-    }
-    public Pokemon(){
+public class Pokemon
+{
+    private int level =0;
+    private int Exp = 0;
 
+    private int HP = 0;
+    private int Damage = 0;
+    private String nameP = ".";
+    private String name = "/////";
+    public static void main(String[] args){
+    
     }
-    public void setName(String name){
+      
+    public String getnameP(){ 
+        return nameP; 
+    }
+
+    public String getname(){ 
+        return name; 
+    }
+
+    public void expup(int exp){
+        Exp += exp;
+    }
+
+    
+
+    public void levelup(){
+        Exp -= 100;
+        level ++;
+        HP += 100;
+        Damage += 10;
+    }
+
+    public  int getexp(){ 
+        return Exp; 
+    }
+
+    public  int getlevel(){ 
+        return level; 
+    }
+
+    public int getHP(){ 
+        return HP; 
+    }
+
+    public int getDamage(){ 
+        return Damage; 
+    }
+
+    public void Name(String name){
         this.name = name;
     }
-    public void getName(){
-        System.out.println(this.name);
+
+    public void catchpokemon(Pokemon pokemon){
+        nameP = pokemon.getnameP();
+        level = pokemon.getlevel();
+        HP = pokemon.getHP();
+        Damage = pokemon.getDamage();
     }
-    /*
-    public static void berryEat(int eat){ 
-        if(eat > 0){
-            System.out.println("Pokemon sleep..zZZ");
+    
+    public void Nopoke()
+    {
+        nameP ="--";
+        level = 0;
+        HP = 0;
+        Damage = 0;
+    }
+   
+    public void poke(int NumP,int n){
+        if(NumP == 0){
+            nameP ="Pikachu";
+            level = n;
+            HP = 100 + (10*(n-1));
+            Damage = 10 + n-1;
         }
-    }
-    public static void lvUP(int pokemonLV){
-        //int i=0;
-        while(pokemonLV != oldLV){
-            HP += 50;
-            MP += 20;
-            atkDMG += 10;
-            oldLV++;
-           // i++;
+        else if(NumP == 1){
+            nameP ="Jigglypuff";
+            level = n;
+            HP = 90 + (5*(n-1));
+            Damage = 6 + n-1;
         }
-        System.out.println("Your pokemon lv.up!");
-        //System.out.println("lv = "+i);
-    }
-    public static void pokemonSTATUS(){
-        System.out.println("HP= "+HP);
-        System.out.println("MP= "+MP);
-        System.out.println("AttackDamage= "+atkDMG);
-        System.out.println("LV= "+pokemonLV);
-    }
-    public static int pokemonFIGHT(int dmg, int hp){
-        while(hp != 0){
-            hp -= atkDMG;
-            HP -= dmg;
-            if(HP <= 0)  
+        else if(NumP == 2){
+            nameP ="Psyduck";
+            level = n;
+            HP = 110 + (5*(n-1));
+            Damage = 4 + n-1;
         }
-    }*/
+        else if(NumP == 3){
+            nameP ="Wartotle";
+            level = n;
+            HP = 100 + (5*(n-1));
+            Damage = 4 + n-1;
+        }
+        else if(NumP == 4){
+            nameP ="Growlithe";
+            level = n;
+            HP = 102 + (5*(n-1));
+            Damage = 1 + n-1;
+        }
+        else if(NumP == 5){
+            nameP ="Flareon";
+            level = n;
+            HP = 110 + (5*(n-1));
+            Damage = 5 + n-1;
+        }
+        
+    }
 }
